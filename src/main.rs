f5d8cc4ee5a75eefabc4eaa205f871555e9757bc
holsee <sholdsworth@gmail.com>
2017-03-09 23:44:20 +0000

    lil tidy

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 extern crate bloom_spell;
-
 use bloom_spell::util::bloom_filter::BloomFilter;
-use std::hash::{Hash, SipHasher, Hasher};
 
 fn main() {
     let bf = BloomFilter::new(10, 2);
     println!("{:?}", bf);
 
     let h = BloomFilter::hash(&"wow");
-
-    println!("{:?}", h);
+    println!("Hash \"wow\": {:?}", h);
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,2 @@
 extern crate bitvec_rs;
 pub mod bloom_filter;
-pub mod fnv;
-
-// Re-export for `bitvec_rs::BitVec` from the `bitvec-rs` crate.
-// pub mod bitvec {
-//     pub use super::bitvec_rs::BitVec;
-// }
